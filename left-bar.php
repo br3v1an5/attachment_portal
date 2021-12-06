be38c24aeaaf6df339964fb15d092bc8653563ed
arnoldwamae <arnoldwamae2@gmail.com>
2021-12-06 19:15:35 +0300

    Added Submissions

---
+++ left-bar.php
@@ -0,0 +1,62 @@
+<div class="left-side-bar">
+    <div class="brand-logo">
+        <a href="index.html">
+            <img src="vendors/images/deskapp-logo.svg" alt="" class="dark-logo">
+            <img src="vendors/images/deskapp-logo-white.svg" alt="" class="light-logo">
+        </a>
+        <div class="close-sidebar" data-toggle="left-sidebar-close">
+            <i class="ion-close-round"></i>
+        </div>
+    </div>
+    <div class="menu-block customscroll">
+        <div class="sidebar-menu">
+            <ul id="accordion-menu">
+
+                <li>
+                    <a href="/" class="dropdown-toggle no-arrow">
+                        <span class="micon dw dw-diagram"></span><span class="mtext">Home</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/attachment-apply.php" class="dropdown-toggle no-arrow">
+                        <span class="micon dw dw-diagram"></span><span class="mtext">Attachment</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="attachment.html" class="dropdown-toggle no-arrow">
+                        <span class="micon dw dw-chat3"></span><span class="mtext">Support</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="attachment.html" class="dropdown-toggle no-arrow">
+                        <span class="micon dw dw-invoice"></span><span class="mtext">log book</span>
+                    </a>
+                </li>
+                <li>
+                    <div class="dropdown-divider"></div>
+                </li>
+                <li>
+                    <div class="sidebar-small-cap">Extra</div>
+                </li>
+                <li>
+                    <a href="javascript:;" class="dropdown-toggle">
+                        <span class="micon dw dw-edit-2"></span><span class="mtext">Documentation</span>
+                    </a>
+                    <ul class="submenu">
+                        <li><a href="introduction.html">Introduction</a></li>
+                        <li><a href="getting-started.html">Getting Started</a></li>
+                        <li><a href="color-settings.html">Color Settings</a></li>
+                        <li><a href="third-party-plugins.html">Third Party Plugins</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="https://dropways.github.io/deskapp-free-single-page-website-template/" target="_blank" class="dropdown-toggle no-arrow">
+                        <span class="micon dw dw-paper-plane1"></span>
+                        <span class="mtext">Landing Page <img src="vendors/images/coming-soon.png" alt="" width="25"></span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="mobile-menu-overlay"></div>
