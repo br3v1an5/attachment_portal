ce65bd9e0f03149c9dd002b2f5b7e046195f1082
= <=>
2022-03-31 09:01:20 +0300

    Breaking Changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,14 +11,92 @@
         {{session()->get('error')}}
     </div>
     @endif
-    <div class="mt-2 mb-4">
-        <div class="col-xs-12 col-sm-8 col-md-6 col-lg-6 ml-auto mr-auto">
-            <div class="col-sm-12 border border-primary shadow rounded bg-white py-2 text-center">
-                <h1>Welcome {{auth()->user()->name}}</h1>
+    <div class="card-box pd-20 height-100-p mb-30">
+        <div class="row align-items-center">
+            <div class="col-md-4">
+                <img src="/assets/vendors/images/banner-img.png" alt="">
+            </div>
+            <div class="col-md-8">
+                <h4 class="font-20 weight-500 mb-10 text-capitalize">
+                    Welcome back <div class="weight-600 font-30 text-blue">{{auth()->user()->name}}</div>
+                </h4>
+                <p class="font-18 max-width-600">{{auth()->user()->email}}</p>
             </div>
         </div>
-        <!--/.col-xs-12 col-sm-8 col-md-6 col-lg-4-->
     </div>
-    <!--/.mt-2 mb-4-->
+    <div class="row">
+        <div class="col-xl-3 mb-30">
+            <div class="card-box height-100-p widget-style1">
+                <div class="d-flex flex-wrap align-items-center">
+                    <div class="progress-data">
+                        <div id="chart"></div>
+                    </div>
+                    <div class="widget-data">
+                        <div class="h4 mb-0"><small>loading..</small></div>
+                        <div class="weight-600 font-14">Supervisors</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 mb-30">
+            <div class="card-box height-100-p widget-style1">
+                <div class="d-flex flex-wrap align-items-center">
+                    <div class="progress-data">
+                        <div id="chart2"></div>
+                    </div>
+                    <div class="widget-data">
+                        <div class="h4 mb-0"><small>loading..</small></div>
+                        <div class="weight-600 font-14">Students</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 mb-30">
+            <div class="card-box height-100-p widget-style1">
+                <div class="d-flex flex-wrap align-items-center">
+                    <div class="progress-data">
+                        <div id="chart3"></div>
+                    </div>
+                    <div class="widget-data">
+                        <div class="h4 mb-0"><small>loading..</small></div>
+                        <div class="weight-600 font-14">Applications</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 mb-30">
+            <div class="card-box height-100-p widget-style1">
+                <div class="d-flex flex-wrap align-items-center">
+                    <div class="progress-data">
+                        <div id="chart4"></div>
+                    </div>
+                    <div class="widget-data">
+                        <div class="h4 mb-0"><small>loading..</small></div>
+                        <div class="weight-600 font-14">Towns</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xl-8 mb-30">
+            <div class="card-box height-100-p pd-20">
+                <h2 class="h4 mb-20">Activity</h2>
+                <div id="chart5"></div>
+            </div>
+        </div>
+        <div class="col-xl-4 mb-30">
+            <div class="card-box height-100-p pd-20">
+                <h2 class="h4 mb-20">Lead Target</h2>
+                <div id="chart6"></div>
+            </div>
+        </div>
+    </div>
 </div>
+@endsection
+
+@section('scripts')
+<script src="/assets/vendors/scripts/process.js"></script>
+<script src="/assets/src/plugins/apexcharts/apexcharts.min.js"></script>
+<script src="/assets/vendors/scripts/dashboard.js"></script>
 @endsection
